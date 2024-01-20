987f8726a9fcbde1b6f716af671cff7e1bad5d6a
André Luiz Pinheiro <digital3i@gmail.com>
2024-01-20 20:07:24 -0300

    Atualizando o banner de consulta ao acervo

--- template-parts/sections-entrada.php
+++ template-parts/sections-entrada.php
@@ -1,4 +1,38 @@
-            <div class="container mt-6 ">
-                <!-- Three columns of text below the carousel -->
-                <h3>Nosso acervo</h3>
-            </div>
+            <!-- Three columns of text below the carousel -->
+                <div class="container">
+                    <div class="row rounder-md justify-content-center align-items-center mt-3 text-white conheca-nosso-acervo">
+                        <div class="col-4 mt-3 p-3 text-uppercase text-center">
+                            <div>
+                                <span class="text-weight-bold text-up-01">conheça nosso</span><br>
+                                <span class="text-weight-bold text-up-07">Acervo</span>  
+                            </div>
+                        </div>
+                        <div class="col-4 text-center align-items-center">
+                            <a href="/institucional" class="br-button secondary mt-2">Institucional</a><br>
+                            <em class="text-down-02 text-center">A gestão do patrimônio imobiliário<br>da União em publicações da SPU</em>
+                            <br><a href="/memoria" class="br-button secondary mt-2">Histórico</a><br>
+                            <span class="text-nowrap text-down-02 "><em>Documentos que registram <br> a história do Brasil</em></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                    <!-- Three columns of text below the carousel -->
+                <div class="container">
+                    <div class="row rounder-md justify-content-center align-items-center mt-3 p-3 text-white conheca-nosso-acervo">
+                        <div class="col-4 mt-3 p-3 text-uppercase text-center">
+                            <div>
+                                <span class="text-weight-bold text-up-01">conheça nosso</span><br>
+                                <span class="text-weight-bold text-up-07">Acervo</span>  
+                            </div>
+                        </div>
+                        <div class="col-2 text-center align-items-center">
+                            <a href="/institucional" class="br-button secondary mt-2">Institucional</a><br>
+                            <em class="text-down-02 text-center">A gestão do patrimônio imobiliário<br>da União em publicações da SPU</em>
+                        </div>
+                        <div class="col-2 text-center align-items-center">
+                            <a href="/memoria" class="br-button secondary mt-2">Histórico</a><br>
+                            <span class="text-nowrap text-down-02 "><em>Documentos que registram <br> a história do Brasil</em></a>
+                        </div>
+                    </div>
+                </div>
